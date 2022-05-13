64d4940b3b157009fdd1419f00afe9cb9a5ae5b3
ldezons <91892129+ldezons@users.noreply.github.com>
2022-05-13 12:04:08 +0200

    Dez Aspp

--- mtl/models/model_parts.py
+++ mtl/models/model_parts.py
@@ -263,25 +263,14 @@ class OrdinalRegression(torch.nn.Module):
         super(OrdinalRegression, self).__init__()
     def forward(self, x):
         N, C, H, W = x.size()
-        ord_num = C // 16
-        #implementation according to the paper
-        A = x[:, ::2, :, :]
-        B = x[:, 1::2, :, :]
-        A = A.reshape(N, 1, ord_num * H * W)
-        B = B.reshape(N, 1, ord_num * H * W)
-        A = A.unsqueeze(dim=1)
-        B = B.unsqueeze(dim=1)
-        concat_feats = torch.cat((A, B), dim=1)
-
+        ord_num = C // 2
+        x = x.view(-1, 2, ord_num, H, W)
         if self.training:
-           prob = F.log_softmax(concat_feats, dim=1)
-           ord_prob = x.clone()
-           ord_prob[:, 0::2, :, :] = prob[:, 0, :, :, :]
-           ord_prob[:, 1::2, :, :] = prob[:, 1, :, :, :]
-           return ord_prob
+            prob = F.log_softmax(x, dim=1).view(N, C, H, W)
+            return prob
 
-        ord_prob = F.softmax(concat_feats, dim=1)[:, 0, ::]
-        ord_label = torch.sum((ord_prob > 0.5), dim=1).reshape((N, 1, H, W))
+        ord_prob = F.softmax(x, dim=1)[:, 0, :, :, :]
+        ord_label = torch.sum((ord_prob > 0.5), dim=1)
         return ord_prob, ord_label
 
 
